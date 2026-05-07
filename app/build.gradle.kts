2959b1511b25578028b713543554f3ad79c11089
Anghi Ramirez <abiramirezpaico03@gmail.com>
2026-05-06 23:57:04 -0500

    integracion login y acceso

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "com.il.apptransporte"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.il.apptransporte"
         minSdk = 25
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
